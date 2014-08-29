548f6914eddfdca7bd12f9e8ddae8af5bcd82658
Saurav Verma <viczsaurav@gmail.com>
2014-08-29 21:24:04 +0800

    Enum

--- QPoolTaxi/src/com/example/common/Gender.java
+++ QPoolTaxi/src/com/example/common/Gender.java
@@ -1,16 +1,9 @@
 package com.example.common;
 
-public enum Gender {
-	
-	
-	
-	
-	M("Male"),F("Female");
-	
-	String gender="";
-	
-	Gender(String s)
-	{
+public enum Gender {	
+	M("Male"),F("Female");	
+	String gender="";	
+	Gender(String s){
 		gender=s;
 	}
 }
